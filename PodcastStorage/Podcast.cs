d5f5899a3ee4a2b3daa7c1f22ed26c4a75760f35
breeze1076 <46266052+breeze1076@users.noreply.github.com>
2018-12-31 12:27:24 +0200

    Language property added

--- PodcastStorage/Podcast.cs
+++ PodcastStorage/Podcast.cs
@@ -12,6 +12,7 @@ namespace PodcastStorage
         internal string Country { get; set; }
         internal PodcastCategory Category { get; set; }
         internal int AverageLength { get; set; }
+        internal string Language { get; set; }
     }
 
     internal enum PodcastCategory
